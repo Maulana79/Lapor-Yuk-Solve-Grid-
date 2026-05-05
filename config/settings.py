d8874671f598fe28fd117ceeca33edb6c45f283b
Muhammad Maulana <lanas9066@gmail.com>
2026-05-05 22:23:07 +0800

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-in-prod
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,lapor-yuk.vercel.app').split(',')
 
 
 # Application definition
